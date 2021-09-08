16cbb26dcc0893d772069fa5b9b3542eae2e6d80
Antti Solonen <antti.solonen@gmail.com>
2021-09-08 09:20:47 +0300

    better performance implementation for kf

--- minkf/methods.py
+++ minkf/methods.py
@@ -13,8 +13,10 @@ def kf_update(y, xp, Cp, K, R):
 
     CpKT = Cp.dot(K.T)
     obs_precision = K.dot(CpKT) + R
-    xest = xp + CpKT.dot(np.linalg.solve(obs_precision, y-K.dot(xp)))
-    Cest = Cp - CpKT.dot(np.linalg.solve(obs_precision), CpKT.T)
+    G = np.linalg.solve(obs_precision.T, CpKT.T).T
+
+    xest = xp + G.dot(y-K.dot(xp))
+    Cest = Cp - G.dot(CpKT.T)
 
     return xest, Cest
 
@@ -27,13 +29,13 @@ def run_filter(y, x0, Cest0, M, K, Q, R, u=None):
 
     # transform inputs into lists of np.arrays (unless they already are)
     Mlist = M if type(M) == list else nobs * [M]
-    Klist = K if type(M) == list else nobs * [M]
-    Qlist = Q if type(M) == list else nobs * [M]
-    Rlist = R if type(M) == list else nobs * [M]
+    Klist = K if type(M) == list else nobs * [K]
+    Qlist = Q if type(M) == list else nobs * [Q]
+    Rlist = R if type(M) == list else nobs * [R]
     ulist = u if type(u) == list else nobs * [u]
 
     # space for saving end results
-    xp_all, Cp_all, xest_all, Cest_all = nobs * [None]
+    xp_all = nobs * [None]
     Cp_all = nobs * [None]
     xest_all = nobs * [None]
     Cest_all = nobs * [None]
@@ -51,8 +53,8 @@ def run_filter(y, x0, Cest0, M, K, Q, R, u=None):
     results = {
         'xp': xp_all,
         'Cp': Cp_all,
-        'xest': xest_all,
-        'Cest': Cest_all,
+        'x': xest_all,
+        'C': Cest_all,
     }
 
     return results
